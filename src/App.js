f00eafa39d9e25f64d924182f865b3ecde6a8eff
mallik0715 <mallik0715@gmail.com>
2024-11-18 00:08:48 +0530

    Initial-Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import LoadMoreData from './components';
+
+
+function App() {
+  return (
+    <div className="App">
+     <LoadMoreData/>
+    </div>
+  );
+}
+
+export default App;
